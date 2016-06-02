3f731e365e936f646534d9dcf5f33028799eaa5e
Delvin Varghese <delvin.friends@gmail.com>
2016-06-02 01:48:12 +0100

    successfull connection to DB and putting whisper_project and associated filenames there

--- app/connect_db.py
+++ app/connect_db.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask.ext.mysql import MySQL
 import config
-
+import datetime
+import MySQLdb
 app = Flask(__name__)
 mysql = MySQL()
 
@@ -38,14 +39,47 @@ def validate_email(email):
     print str(output)
     return output
 
+def get_id_from_project(username, title):
+    query_a = "select project_id from BucketList.tbl_projects where title='{}' and user_username='{}'"
+    query_b = query_a.format(title, username)
+    cursor.execute(query_b)
+    output = cursor.fetchone()
+    print output[0]
+    return output[0]
+
+
+def add_new_whisper(username, title, current_timestamp):
+    query = "INSERT INTO BucketList.tbl_projects(user_username, title, origin_date, status) VALUES('{}', '{}', '{}', 'started')"
+    query_1 = query.format(username, title, current_timestamp)
+    try:
+        cursor.execute(query_1)
+        conn.commit()
+        print str(username) + " inserted successfully.."
+        return get_id_from_project(username, title)
+    except MySQLdb.IntegrityError, e:
+        print "Not done " + str(e.args)
+        conn.rollback()
+        print e.message
+        return None
+
 
 # TODO: currently a copy of above method, make it more relevant..
-def add_file_details_to_db(filename, project_id, media_type, user_username):
-    get_users_query = "INSERT INTO BucketList.tbl_media(filename, project_id, media_type, user_username) VALUES('{}','{}','{}','{}') "
-    final_query = get_users_query.format([filename, project_id, media_type, user_username])
-    cursor.execute(final_query)
-    output = cursor.fetchall()
-    if len(output) > 0:
-        print "SQL has output"
-    print str(output)
-    return output
+def add_file_details_to_db(filename, project_id, media_type, username, current_timestamp):
+    get_users_query = "INSERT INTO BucketList.tbl_media(filename, project_id, media_type, user_username, origin_date) VALUES('{}','{}','{}','{}', '{}') "
+    final_query = get_users_query.format(filename, project_id, media_type, username, current_timestamp)
+
+    try:
+        cursor.execute(final_query)
+        conn.commit()
+        print str(username) + " inserted successfully.."
+        return True
+    except MySQLdb.IntegrityError, e:
+        print "Not done " + str(e.args)
+        conn.rollback()
+        print e.message
+        return False
+
+if __name__ == '__main__':
+    current_timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+    # a = add_new_whisper("a@d", "my_diaries_7", current_timestamp)
+    b = add_file_details_to_db("blah.mp3","19","audio","a@d",current_timestamp)
--- app/views_dir/whispers_manager.py
+++ app/views_dir/whispers_manager.py
@@ -3,6 +3,9 @@ from app import app
 import os
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
+import datetime
+from .. import connect_db
+
 import uuid
 uuid_generator = uuid.uuid4()
 
@@ -27,6 +30,7 @@ def upload_file():
             return redirect('/startWhisper')
         image_file = request.files['file']
         audio_file = request.files['audio_file']
+        whisper_title = request.form['title']
         print str(audio_file)
         print "File found?..."
         # if user has not selected file, browser will also submit an empty part without filename.
@@ -44,12 +48,23 @@ def upload_file():
             new_image = gen_filename + image_extension
             new_audio = gen_filename + audio_extension
 
+            # Save the files to disk.
             image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_image))
             audio_file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_audio))
             print "Files generated: " + str(new_audio) + ", " + str(new_image)
 
+            current_timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+            username = session['user']
+            whisper_id = connect_db.add_new_whisper(username, whisper_title, current_timestamp)
+            audio_to_db = connect_db.add_file_details_to_db(new_audio,whisper_id,"audio",username,current_timestamp)
+            image_to_db = connect_db.add_file_details_to_db(new_image,whisper_id,"image",username,current_timestamp)
+            if audio_to_db:
+                print "audio saved to database.."
+            if image_to_db:
+                print "image saved to database.."
             return redirect(url_for('track_whispers'))
     return render_template('userHome.html')
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
